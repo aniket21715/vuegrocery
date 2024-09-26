7c92e136b3e3c23daa8a4859c367cd8c3c719928
aniket21715 <142142736+aniket21715@users.noreply.github.com>
2024-09-26 22:38:03 +0530

    Update StoreManagerRegister.vue

--- frontend/src/components/StoreManagerRegister.vue
+++ frontend/src/components/StoreManagerRegister.vue
@@ -64,7 +64,7 @@
     methods: {
       async StoreManagerRegister() {
         try {
-          const response = await fetch('http://localhost:5000/register/store-manager', {
+          const response = await fetch('https://vuegrocery.onrender.com/register/store-manager', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
