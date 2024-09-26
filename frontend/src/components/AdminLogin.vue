255efbaa9ff3af815979bf0507bd2957148245ce
aniket21715 <142142736+aniket21715@users.noreply.github.com>
2024-09-26 22:32:19 +0530

    Update AdminLogin.vue

--- frontend/src/components/AdminLogin.vue
+++ frontend/src/components/AdminLogin.vue
@@ -51,7 +51,7 @@
       async login() {
         try {
           // Send login request to the Flask backend using fetch
-          const response = await fetch('http://localhost:5000/login', {
+          const response = await fetch('https://vuegrocery.onrender.com/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
