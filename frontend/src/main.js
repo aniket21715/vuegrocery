6dffb89203b8633b10004288e999482471742944
aniket21715 <142142736+aniket21715@users.noreply.github.com>
2024-09-27 19:15:31 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,10 @@
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faFacebook, faGoogle, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import '@mdi/font/css/materialdesignicons.css';
 library.add(faFacebook, faGoogle, faInstagram);
 
